32fd55dee60ea4969fb8c939e8d7a16e8d0f7fb5
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2025-01-30 09:04:41 +0600

    http

--- Basics/Node/00_GettingStarted/http.js
+++ Basics/Node/00_GettingStarted/http.js
@@ -1,1 +1,21 @@
 const http = require('http');
+
+// const server = http.createServer( (req, res) => {
+//     res.write('Hello World');
+//     res.end();
+// });
+
+const server = http.createServer( (req, res) => {
+    if(req.url === '/') {
+        res.write('Hello World');
+        res.end();
+    } else if (req.url === '/about') {
+        res.write('About Page');
+        res.end();
+    } else {
+        res.write('Page not found');
+        res.end();
+    }
+});
+
+server.listen(3000);
