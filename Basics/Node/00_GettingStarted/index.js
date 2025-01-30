64d77f077768663ab6c9a9770cb98a2ae8bda4f1
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2025-01-30 07:10:29 +0600

    Global Object & Module System, export and import

--- Basics/Node/00_GettingStarted/index.js
+++ Basics/Node/00_GettingStarted/index.js
@@ -1,2 +1,8 @@
-const a = 5;
-console.log(a);
+const Others = require('./other')
+const _ = require('lodash');
+
+console.log(Others.name);
+console.log(Others.people);
+console.log(Others.number);
+
+console.log(`Last array of people using => lodash: "_last" => ${_.last(Others.people)}`);
---
+++ Basics/Node/00_GettingStarted/other.js
@@ -0,0 +1,9 @@
+const people = ['nahin', 'mim', 'soyeba']
+const name = "Hasnat Shahriyar";
+const number = 2025;
+
+module.exports = {
+    people,
+    name,
+    number
+};
