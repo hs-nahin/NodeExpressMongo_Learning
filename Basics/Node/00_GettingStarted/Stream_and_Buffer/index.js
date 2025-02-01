1c0be84a7f539d0a57d4b8b14de5ae09deb45167
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2025-02-02 02:11:12 +0600

    using pipe method

--- Basics/Node/00_GettingStarted/Stream_and_Buffer/index.js
+++ Basics/Node/00_GettingStarted/Stream_and_Buffer/index.js
@@ -1,8 +1,24 @@
 const fs = require('fs');
+const http = require('http');
 
-const ourReadStream = fs.createReadStream(`${__dirname}/bigdata.txt`);
-const ourWriteStream = fs.createWriteStream(`${__dirname}/output.txt`);
+// const ourReadStream = fs.createReadStream(`${__dirname}/bigdata.txt`);
+// const ourWriteStream = fs.createWriteStream(`${__dirname}/output.txt`);
 
-ourReadStream.on('data', (chunk) => {
-    ourWriteStream.write(chunk);
+// Reading data from bigdata.txt and writing it to output.txt
+// ourReadStream.on('data', (chunk) => {
+//     ourWriteStream.write(chunk);
+// });
+
+// Using pipe method
+// ourReadStream.pipe(ourWriteStream);
+
+// Creating a server
+const server = http.createServer((req, res) => {
+    const myReadStream = fs.createReadStream(__dirname + '/bigdata.txt', 'utf-8');
+    myReadStream.pipe()
 });
+
+server.listen(3000, 'localhost');
+console.log('Server is running on port 3000');
+
+// NOTE : 'req' is a readable stream and 'res' is a writable stream. So, we can use pipe method to read data from readable stream and write it to writable stream.
