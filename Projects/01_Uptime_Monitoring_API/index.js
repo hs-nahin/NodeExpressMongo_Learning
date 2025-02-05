4046667e23d6f1a4cb5ac2ef3923acd429441e7f
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2025-02-06 04:22:59 +0600

    get and post data

--- Projects/01_Uptime_Monitoring_API/index.js
+++ Projects/01_Uptime_Monitoring_API/index.js
@@ -5,6 +5,10 @@
 
 // Dependencies
 const http = require('http');
+const url = require('url');
+const {StringDecoder} = require('string_decoder');
+const { buffer } = require('stream/consumers');
+
 
 // app object - module scaffolding
 const app = {};
@@ -24,8 +28,26 @@ app.createServer = () => {
 
 // Handle requests and send response
 app.handleRequestRes = (req, res) => {
-    // Send response
-    res.end('কিরে মেহেদী কি খবর?');
+    // Request Handling
+    // Get the url and parse it
+    const parsedUrl = url.parse(req.url, true);
+    const path = parsedUrl.pathname;
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+    const method = req.method.toLowerCase();
+    const queryStringObject = parsedUrl.query;
+    const headersObject = req.headers;
+    const decoder = new StringDecoder('utf-8');
+    let realData = '';
+
+    req.on('data', (buffer) => {
+        realData += decoder.write(buffer);
+    })
+    req.on('end', () => {
+        realData += decoder.end();
+        console.log(realData); 
+        // Send response
+        res.end('Hello Nahin');
+    })
 }
 
 // Start the server
